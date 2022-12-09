dddd5000587d34880280314708352cf0861dfa72
Pham Nhat Tien <104922737+TienPham98@users.noreply.github.com>
2022-12-10 02:23:24 +0700

    Update CheckoutForm.js

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -22,7 +22,7 @@ function CheckoutForm() {
     if (!stripe || !elements || user.cart.count <= 0) return;
     setPaying(true);
     const { client_secret } = await fetch(
-      "http://localhost:8080/create-payment",
+      "https://mern-ecom-app-api.onrender.com/create-payment",
       {
         method: "POST",
         headers: {
